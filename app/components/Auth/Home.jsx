b6c11f3f03217959a6cef052e8613b70ef9808e7
Anish <anish15.786@gmail.com>
2024-02-04 10:45:48 +0530

    first commit

---
+++ app/components/Auth/Home.jsx
@@ -0,0 +1,20 @@
+
+import React, { use } from 'react';
+import styles from './Auth.module.css'
+import { Button } from '@mui/material';
+import { Input } from 'antd';
+import { Container } from 'react-bootstrap';
+
+const AuthScreen = () => {
+  return (
+    <div className={styles.Wrapper}>
+        <Container>
+        <Button variant='contained'>ekfwbkf</Button>
+  
+        </Container>
+      
+    </div>
+  )
+}
+
+export default AuthScreen
--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,13 @@
-import { Inter } from "next/font/google";
-import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+import { Montserrat } from "next/font/google";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./globals.css";
+import { AntdRegistry } from '@ant-design/nextjs-registry';
+const inter = Montserrat({ 
+  subsets: ["latin"],
+  weight:["100","300","400","500","600","700","900"],
+  variable:"--globalFont"
+ });
 
 export const metadata = {
   title: "Create Next App",
@@ -10,8 +16,12 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className='dark'>
+      <body className={inter.className}>
+        <AntdRegistry>
+        {children}
+        </AntdRegistry>
+        </body>
     </html>
   );
 }
